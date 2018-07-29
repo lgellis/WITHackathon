7927c361b45d5c31d47ae2a1cbb2e455bc88fc64
Kate LaMendola <kjlamendus.ibm.com@Kates-MacBook-Pro.local>
2018-07-29 14:50:13 -0500

    updated with counties and regions in drop down

--- firePredition/server.R
+++ firePredition/server.R
@@ -6,21 +6,35 @@
 # 
 #    http://shiny.rstudio.com/
 #
-
+#install.packages("data.table")
 library(shiny)
+# library(data.table)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
+
+  # Send a pre-rendered image, and don't delete the image after sending it
+  output$preImage <- renderImage({
+    # When input$n is 3, filename is ./images/image3.jpeg
+    filename <- normalizePath(file.path('./images',
+                                        paste('image', input$n, '.jpeg', sep='')))
+  
+    # Return a list containing the filename and alt textxs
+    list(src = filename,
+         width = 260,
+         height = 260,
+         alt = paste("Image number", input$n))
+    
+  }, deleteFile = FALSE)
+  
   output$distPlot <- renderPlot({
-    
+
     # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
+    x    <- faithful[, 2]
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
+
     # draw the histogram with the specified number of bins
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+
   })
-  
 })
--- firePredition/ui.R
+++ firePredition/ui.R
@@ -3,31 +3,47 @@
 # run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
-
 library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
+  br(),
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  headerPanel("Fire Disaster Prediction System"),
   
-  # Sidebar with a slider input for number of bins 
+  # df_counties <-fread('https://raw.githubusercontent.com/lgellis/WITHackathon/master/files/counties.csv', stringsAsFactors = FALSE),
+  df_counties <-fread('./data/counties.csv', stringsAsFactors = FALSE),
+  df_regions <-fread('./data/regions.csv', stringsAsFactors = FALSE),
+  
+  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+      selectInput('region_id', label = 'Select Region', c(Choose = '', df_regions[1]), selectize = FALSE),
+      selectInput('element_id', label = 'Select County', c(Choose = '', df_counties[1]), selectize = FALSE),
+      dateInput(
+        'date_input_id',
+        label = 'Disaster Start Date',
+        value = NULL,
+        min = NULL,
+        max = NULL,
+        format = "yyyy-mm-dd",
+        startview = "month",
+        weekstart = 0,
+        language = "en",
+        width = NULL,
+        autoclose = TRUE
+      )
     ),
-    
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+      uiOutput("toColumns"),
+      # h1('The title of some text'),
+      imageOutput("preImage"),
+      p('Recommendation will be here...')
+      # ,plotOutput("distPlot")
     )
   )
 ))
